444d0b9315097b346f6f90038cd6735f2fd8e3e6
ncduongg <ncduongg@gmail.com>
2021-12-03 15:25:21 +0700

    update mui gio

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const connect = async () => {
-    const uri2 = "mongodb://localhost:27017/test_dev"
-    try {
-        await mongoose.connect(uri2);
-        console.log("connect ok");
-    } catch (error) {
-        console.log("meo duoc");
-
-    }
-}
-module.exports = { connect }
+  const uri2 = "mongodb://128.199.144.198:27017/test_dev";
+  try {
+    await mongoose.connect(uri2);
+    console.log("connect ok");
+  } catch (error) {
+    console.log("meo duoc");
+  }
+};
+module.exports = { connect };
